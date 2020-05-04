6900d1468372d6b88858358963ba15c4ef36364b
Djodane <romain.sinet@gmail.com>
2020-05-04 17:56:09 +0200

    test api

--- deploy/test-api.sh
+++ deploy/test-api.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-keyword="student_ages" # enter the keyword for test content
+keyword="student_ages" 
 if curl -u toto:python -X GET http://192.168.48.10:5001/pozos/api/v1.0/get_student_ages | grep "$keyword"
 then
-    # if the keyword is in the content
     exit 0
 else
     exit 1
