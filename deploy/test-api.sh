ba56e3aef58e70a1d20b3ee527a52fbe90cb890e
Djodane <romain.sinet@gmail.com>
2020-06-03 23:20:56 +0200

    modified jenkinsfile

--- deploy/test-api.sh
+++ deploy/test-api.sh
@@ -1,5 +1,5 @@
 keyword="student_ages" 
-if curl -u toto:python -X GET http://192.168.48.10:5001/pozos/api/v1.0/get_student_ages | grep "$keyword"
+if curl -u toto:python -X GET http://192.168.48.10:5002/pozos/api/v1.0/get_student_ages | grep "$keyword"
 then
     exit 0
 else
